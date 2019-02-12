1e6ad6f9b93ae3a16d2b3ada16b7129f32d97e03
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2019-02-12 01:45:43 -0500

    Added install file

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IMAGE=singularity-ghcal
+DIRECTORY=~/.singularity
+
+if [ ! -d $DIRECTORY ]; then
+	mkdir $DIRECTORY
+fi
+
+if [ ! -d ~/bin ]; then
+	mkdir ~/bin
+fi
+
+if [ ! -f $DIRECTORY/$(echo $IMAGE | cut -d"-" -f2).simg ]; then
+	singularity pull --name $(echo $IMAGE | cut -d"-" -f2).simg shub://icaoberg/$IMAGE
+	mv -v $(echo $IMAGE | cut -d"-" -f2).simg $DIRECTORY
+fi
+
+cat << EOF > ~/bin/ghcal
+#!/bin/bash
+
+singularity run --app ghcal ~/.singularity/$(echo $IMAGE | cut -d"-" -f2).simg $1
+EOF
+
+chmod +x ~/bin/ghcal
